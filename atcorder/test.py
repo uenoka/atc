fe7e462dc46046548e1ad9ef4b0c4387c458bdf3
uenoka <ueno.k.0505@gmail.com>
2020-05-05 00:47:16 +0900

    solve some problem

--- atcorder/test.py
+++ atcorder/test.py
@@ -1,2 +1,14 @@
-for i in range(1,10000):
-    print(i*2019)
+n,m,p = map(int,input().split())
+#ans = 1
+#for i in range(n,m):
+#    ans *= i
+#    print(ans)
+#print("last mod ",ans%p)
+
+ans = 1
+for i in range(n,m):
+    ans %= p
+#    print(ans)
+    ans *= i
+#    print(ans)
+print("each mod ",ans%p)
