1443f450d9e79e50a5ee55ef23560c2ed705510d
ka_ueno <ka.ueno@acca-int.co.jp>
2020-05-28 18:50:17 +0900

    solving ALDS1_5_A.py

--- atcorder/ALDS1_5_A.py
+++ atcorder/ALDS1_5_A.py
@@ -1,13 +1,23 @@
 # ALDS1_5_A.py
-def rec(m, a):
-    if len(a) == len(A):
-        return True
+memo = []
 
-    pass
+
+def rec(m, a, idx, id):
+    if a == m:
+        return True
+    if idx >= len(A):
+        return False
+    _next = A[idx]
+    return rec(m, a+_next, idx+1, id+"1") or rec(m, a, idx+1, id+"0")
 
 
 N = int(input())
 A = list(map(int, input().split()))
+input()
 M = list(map(int, input().split()))
 
-print(rec(M[0], []))
+for i in M:
+    if rec(i, 0, 0, "0"):
+        print('yes')
+    else:
+        print('no')
