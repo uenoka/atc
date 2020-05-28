cd430e968fbf1ce5e7e8e7f1a4259627ad66423e
uenoka <ueno.k.0505@gmail.com>
2020-05-28 19:48:36 +0900

    solving some problems

--- atcorder/ALDS1_5_A.py
+++ atcorder/ALDS1_5_A.py
@@ -1,13 +1,17 @@
 # ALDS1_5_A.py
-def rec(m, a):
-    if len(a) == len(A):
-        return True
-
-    pass
+def rec(m, use, idx):
+    if idx<=len(A):
+        if sum(use)==m:
+            return True
+        use.push(A[idx+1])
+        rec(m,use,idx+1)
+        use.pop()
+        rec(m,use,idx+1)
+    
 
 
 N = int(input())
 A = list(map(int, input().split()))
 M = list(map(int, input().split()))
 
-print(rec(M[0], []))
+print(rec(M[0], [],0))
