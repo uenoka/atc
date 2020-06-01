155ac5bacb44656ad368362230f323d057023de5
uenoka <ueno.k.0505@gmail.com>
2020-06-01 21:47:11 +0900

    wrote blog (abc169 d)

--- test.py
+++ test.py
@@ -2,21 +2,12 @@
 input : 数字
 return : a^p,b^q,...,c^r を {a:p,b:q,...,c:r} の dict で表したもの
 '''
-def can_div(z):
-    target = [2,3,4,5,7,8,9,11,13,16,17,19,23,25,27,31,36,37,41,47,49]
-    if z in target:
-        return True
-    return False
 
-def choice(_min,n):
-    z = _min
-    while True:
-        z+=1
-        if z%N != 0:
-            continue
-        if not can_div(z):
-            continue
-    # z はNで割り切れる
-    # z は素数のe乗で表せる
-    # もし選べなかったら-1でbreak条件にする
-    return z
+top = 10**12
+ans = 1
+cnt=0
+while ans<top:
+    ans*=2
+    cnt+=1
+print(cnt,ans)
+print(2**40)
