cc470dd204cb7d6c439f97750bf3f43fea1cce8e
ka_ueno <ka.ueno@acca-int.co.jp>
2020-06-10 18:06:09 +0900

    solved abc117_b.py

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ return : a^p,b^q,...,c^r を {a:p,b:q,...,c:r} の dict で表したもの
 
 top = 10**12
 ans = 1
-cnt=0
-while ans<top:
-    ans*=2
-    cnt+=1
-print(cnt,ans)
+cnt = 0
+while ans < top:
+    ans *= 2
+    cnt += 1
+print(cnt, ans)
 print(2**40)
