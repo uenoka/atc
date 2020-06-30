52451323fbe0319dee77cbf4d3e70a0e35b52dec
uenoka <ueno.k.0505@gmail.com>
2020-06-30 22:26:34 +0900

    solving abc166_c.py

--- atcoder/abc166_c.py
+++ atcoder/abc166_c.py
@@ -4,3 +4,13 @@
 高さが同じ場合を考える必要があるかもしれないが、結局のところ「連結されたグラフ内で最大の値を探し出す」問題。
 ということはいくつ連結グラフがあるかだけでいい…?ここがコーナーケースかな?
 '''
+N, M = map(int,input().split())
+H = list(map(int,input().split()))
+glaph = [[] for i in range(N)]
+for i in range(M):
+    a,b = map(int,input().split())
+    glaph[a-1].append(b-1)
+    glaph[b-1].append(a-1)
+print(glaph)
+seen = [False]*N
+cnt = 0
