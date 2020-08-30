9b1d8e72afc70f80e775602916e265d9d9b7f761
ka_ueno <ka.ueno@acca-int.co.jp>
2020-08-30 16:01:14 +0900

    solved

--- leetcode/find-numbers-with-even-number-of-digits.py
+++ leetcode/find-numbers-with-even-number-of-digits.py
@@ -1,4 +1,7 @@
 # find-numbers-with-even-number-of-digits.py
+import math
+
+
 class Solution:
     def findNumbers(self, nums) -> int:
         ans = 0
@@ -7,6 +10,17 @@ class Solution:
             if len(s) % 2 == 0:
                 ans += 1
         return ans
+    '''
+    log を使うことで桁数を判定できるから少し早いっぽい。
+    とはいえ60ms -> 56 ms だからオーダー変わるほどではななさそう。
+    '''
+
+    def findNumbers2(self, nums) -> int:
+        result = 0
+        for num in nums:
+            if(int(math.log10(num)) % 2 == 1):
+                result += 1
+        return result
 
 
 sol = Solution()
