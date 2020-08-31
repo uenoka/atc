280a9b204f7c2af194d2dcd96e044a41c9ebdc58
ka_ueno <ka.ueno@acca-int.co.jp>
2020-08-31 10:08:25 +0900

    solved

--- leetcode/unique-morse-code-words.py
+++ leetcode/unique-morse-code-words.py
@@ -2,22 +2,21 @@
 
 
 class Solution:
+
     def makeMorseWords(self, words):
         morseCode = {"a": ".-", "b": "-...", "c": "-.-.", "d": "-..", "e": ".", "f": "..-.", "g": "--.", "h": "....", "i": "..", "j": ".---", "k": "-.-", "l": ".-..", "m": "--",
                      "n": "-.", "o": "---", "p": ".--.", "q": "--.-", "r": ".-.", "s": "...", "t": "-", "u": "..-", "v": "...-", "w": ".--", "x": "-..-", "y": "-.--", "z": "--.."}
-        morsedWords = []
+        morsedWords = set()
         for word in words:
             morsedWord = ""
             for char in word:
                 morsedWord += morseCode[char]
-            morsedWords.append(morsedWord)
+            morsedWords.add(morsedWord)
         return morsedWords
 
     def uniqueMorseRepresentations(self, words) -> int:
         morsedWords = self.makeMorseWords(words)
-        import collections
-        C = collections.Counter(morsedWords)
-        return len(C)
+        return len(morsedWords)
 
 
 sol = Solution()
