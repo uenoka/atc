d7236f3f76f0028d3d441e70838da7cbfa6dc2d9
ka_ueno <ka.ueno@acca-int.co.jp>
2020-08-31 09:40:03 +0900

    solved

--- leetcode/number-of-students-doing-homework-at-a-given-time.py
+++ leetcode/number-of-students-doing-homework-at-a-given-time.py
@@ -1,9 +1,14 @@
 # number-of-students-doing-homework-at-a-given-time.py
+'''
+なぜか同じコードで11%の速度だったり、94%だったりするから、同じオーダーだったらそんなに気にしなくてもよさそう。
+'''
+
+
 class Solution:
     def busyStudent(self, startTime, endTime, queryTime: int) -> int:
         cnt = 0
         for i in range(len(startTime)):
-            if startTime[i] <= queryTime <= endTime[i]:
+            if startTime[i] <= queryTime and queryTime <= endTime[i]:
                 cnt += 1
         return cnt
 
