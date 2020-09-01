7c8a87d5e164f5305e20ad941b7fbdf0ace02e75
KazukiUeno <ueno.k.0505@gmail.com>
2020-09-01 14:23:45 +0900

    solve first unique character

---
+++ leetcode/first-unique-character-in-a-string.py
@@ -0,0 +1,13 @@
+# first-unique-character-in-a-string.py
+'''
+python だったら Collection.counter でやるのが楽そう。
+それか dict を前処理で作ってあげて、元の文字列で全探索
+'''
+from collections import Counter
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        counter = Counter(list(s))
+        for i,c in enumerate(s):
+            if counter[c]==1:
+                return i
+        return -1
