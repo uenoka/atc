17c7daec8844e2c4d2447ffad3d7e1df662ddcbb
ka_ueno <ka.ueno@acca-int.co.jp>
2020-10-28 18:08:28 +0900

    .

--- atcoder/arc106_b.py
+++ atcoder/arc106_b.py
@@ -9,4 +9,34 @@ glaph = [[] for i in range(N+1)]
 for i in range(M):
     c, d = map(int, input().split())
     glaph[c].append(d)
-seen = [False]*N
+    glaph[d].append(c)
+# print(glaph)
+ans = True
+seen = [False]*(N+1)
+for i in range(1, N+1):
+    stack = []
+    connected_node = []
+    if not seen[i]:
+        seen[i] = True
+        stack.append(i)
+        connected_node.append(i)
+    while stack:
+        idx = stack.pop()
+        for node in glaph[idx]:
+            if not seen[node]:
+                connected_node.append(node)
+                stack.append(node)
+                seen[node] = True
+    A_sum = 0
+    B_sum = 0
+    # print(connected_node)
+    for idx in connected_node:
+        A_sum += A[idx-1]
+        B_sum += B[idx-1]
+    if not A_sum == B_sum:
+        ans = False
+        break
+if ans:
+    print('Yes')
+else:
+    print('No')
--- atcoder/test.py
+++
@@ -1,5 +0,0 @@
-N = 9
-ans = 1
-for i in range(2,N):
-    ans*=i
-print(ans)
