93ae432dd2815e65b7ad9f05a3552d8e004ba6e2
ka_ueno <ka.ueno@acca-int.co.jp>
2020-10-31 14:46:54 +0900

    .

--- aoj/DPL_1_B.py
+++ aoj/DPL_1_B.py
@@ -1,12 +1,19 @@
 # DPL_1_B.py
 
-def search(i,u):
-    pass
-N, W = map(int,input().split())
-V = []
-W = []
-for i in range(N):
-    v,w = map(int,input().split())
+n, wmax = map(int, input().split())
+V = [0]
+W = [0]
+for i in range(n):
+    v, w = map(int, input().split())
     V.append(v)
     W.append(w)
-
+# dp = [[0 for i in range(W+1)] for i in range(N+1)]
+dp = [[0 for i in range(wmax+1)] for i in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, wmax+1):
+        if j < W[i]:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-W[i]]+V[i])
+# print(dp)
+print(dp[n][wmax])
