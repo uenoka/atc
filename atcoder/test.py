52950236366ffb57788079498333516fa8e77cff
ka_ueno <ka.ueno@acca-int.co.jp>
2020-11-03 17:23:41 +0900

    solved PAST F

--- atcoder/test.py
+++ atcoder/test.py
@@ -1,9 +1,4 @@
-for i in range(10):
-    if 6**i < 100000:
-        print(i, 6**i)
-print()
-for i in range(10):
-    if 9**i < 100000:
-        print(i, 9**i)
-a = [i for i in range(1, 91)]
-print(a)
+ans = 1
+for i in range(1, 10):
+    ans *= i
+print(ans)
