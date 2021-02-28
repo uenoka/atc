a696bc8d46b5836a6b8e2c71d0325fef2ebcafae
ka_ueno <ka.ueno@acca-int.co.jp>
2021-02-28 10:35:46 +0900

    .

--- leetcode/house-robber-ii.py
+++ leetcode/house-robber-ii.py
@@ -1,11 +1,29 @@
 # house-robber-ii.py
 '''
 動的計画法かなー？
+写経してみた。
+先頭と末尾を外してそれぞれ最大になるようにDPしていく感じ。
+そもそも先頭と末尾を外して出来るかってのが良くわからないなぁ…
 '''
 
 class Solution:
     def rob(self, nums) -> int:
-        return 0
+        def houseRobber(nums):
+            dp = [0] * len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[0],nums[1])
+            for i in range(2,len(nums)):
+                dp[i] = max(dp[i-1],nums[i] + dp[i-2])
+            return dp[-1]
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
+        return max(houseRobber(nums[:-1]), houseRobber(nums[1:]))
+
+
 
 test_cases = [
     [0],
