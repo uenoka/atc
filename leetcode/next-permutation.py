2181a38a1f0e06b39da034147005c1cee5c6c9db
ka_ueno <ka.ueno@acca-int.co.jp>
2021-03-02 17:46:56 +0900

    .

--- leetcode/next-permutation.py
+++ leetcode/next-permutation.py
@@ -3,25 +3,40 @@
 ちょっといれ変えればいいかと思ったが意外と違った…
 単調非減少でなくなる箇所（後ろから増減を見て初めて減少に転じる箇所）と増加箇所でちょうどその減少分の値を超えた個所を入れ替える
 その後下の部分をreverse してあげればいい
-単調非減少を探すのはO(1), 減少個所を丁度超える箇所を見つけるのは二分探索でO(logN)、入れ替えでO(1) , 逆順でO（N）
+単調非減少を探すのはO(N), 減少個所を丁度超える箇所を見つけるのは二分探索でO(logN)、入れ替えでO(1) , 逆順でO（N）なので全体としてはO(N)で解くことができる。
+ごみのようにこういう添え字系苦手だな…頭の中でこういうのが考えられて実装も詰まらずできるようになりたい…
 '''
 class Solution:
     def nextPermutation(self, nums) -> None:
-        flg = False
-        for i in reversed(range(1,len(nums))):
-            if nums[i]>nums[i-1]:
-                nums[i], nums[i-1] = nums[i-1], nums[i]
-                flg = True
-                break
-        if not flg:
-            nums = reversed(nums) 
+        i = len(nums)-1
+        while i>0 and nums[i]<=nums[i-1]:
+            i -= 1
+        if i==0 : 
+            nums.reverse()
+            return
+        targetIdx = i-1
+        j = len(nums)-1
+        while j>0 and nums[targetIdx] >= nums[j]:
+            j-=1
+        swapIdx = j
+        print(targetIdx,swapIdx)
+        print('bef',nums)
+        nums[targetIdx], nums[swapIdx] = nums[swapIdx], nums[targetIdx]
+        print('aft',nums)
+        end = len(nums)-1
+        targetIdx+=1
+        while end > targetIdx:
+            nums[targetIdx], nums[end] = nums[end], nums[targetIdx]
+            targetIdx+=1
+            end-=1
         print(nums)
 
 testcases = [
-    [1,2,3,4,5],
-    [1,3,2,45,56],
-    [5,4,3,2,1],
-    [1,1,2,2,2,2,2,3],
+    [1,1,4,5,4,3,2,1], 
+    [1, 3, 2],
+    [4,3,2,1],
+    [1, 1],
+    [5, 1, 1]
 ]
 for nums in testcases:
     Solution().nextPermutation(nums)
