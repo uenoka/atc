4b492ae9072e9aee61ab41d7022d0472789cec45
ka_ueno <ka.ueno@acca-int.co.jp>
2021-06-21 11:34:49 +0900

    add util function

---
+++ rosalind/file_reader.py
@@ -0,0 +1,14 @@
+'''
+テストデータを読み込む処理
+input : file_name:str -> 各テストケースを特定する文字列
+output : List[str] -> テストケースを配列に変換して返す
+'''
+import os
+
+def readFile(file_name):
+    file_name = file_name.replace('.py','')
+    print(file_name)
+    dir = os.path.dirname(__file__)
+    file = os.path.join(dir, 'rosalind_'+file_name+'.txt')
+    with open(file, "r") as file:
+        return file.readlines()
