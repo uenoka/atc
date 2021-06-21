5bde3e8e5b0b07dcfe9a4760db30aa7e0322ce7a
ka_ueno <ka.ueno@acca-int.co.jp>
2021-06-21 10:42:14 +0900

    solve rosalind

--- rosalind/OverlapGraphs/Overlap_Graphs.py
+++ rosalind/OverlapGraphs/Overlap_Graphs.py
@@ -1,13 +1,40 @@
 import os
+from typing import Sequence
 
 def readInput():
     dir = os.path.dirname(__file__)
     file = os.path.join(dir, 'rosalind.txt')
     with open(file, "r") as file:
-        line = file.readlines()
-        pass
+        return file.readlines()
+
+def trim(str):
+    return str.replace('\n', '').replace('>', '').replace(' ', '')
+
+def prepareData(lis):
+    header = trim(lis[0])
+    sequence = ''
+    ret = []
+    for i in lis[1:]:
+        if i.startswith('>'):
+            ret.append((trim(header), trim(sequence)))
+            header = i
+            sequence = ''
+        else:
+            sequence += i
+    ret.append((trim(header), trim(sequence)))
+    header = i
+    return ret
 
 def solve():
-    pass
-
+    fasta_data = prepareData(readInput())
+    print(fasta_data)
+    ans = []
+    for fasta1 in fasta_data:
+        for fasta2 in fasta_data:
+            if fasta1==fasta2:
+                continue
+            if fasta1[1][-3:] == fasta2[1][:3]:
+                ans.append((fasta1[0], fasta2[0]))
+    for i in ans:
+        print(i[0],i[1])
 solve()
