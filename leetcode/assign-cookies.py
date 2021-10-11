c9bf4c1da7cf9da1b53f6cf83f1a1e9c134fece7
ka_ueno <ka.ueno@acca-int.co.jp>
2021-10-11 12:51:18 +0900

    solve problem

--- leetcode/assign-cookies.py
+++ leetcode/assign-cookies.py
@@ -1,8 +1,27 @@
 # assign-cookies.py
+'''
+愚直に全探索で実装。
+two pointers で O(N) に計算量削減ができる
+'''
 from typing import List
-import collections
 
 class Solution:
+    def findContentChildren2(self, g: List[int], s: List[int]) -> int:
+        ans = 0
+        g.sort()
+        s.sort()
+        gidx = 0
+        sidx = 0
+        while gidx < len(g) and sidx < len(s):
+            if g[gidx] <= s[sidx]:
+                ans += 1
+                gidx += 1
+                sidx += 1
+            else:
+                sidx += 1
+
+        return ans
+
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         ans = 0
         g.sort()
@@ -21,5 +40,5 @@ testcases = [
     [[1, 2, 3],[3]]
 ]
 for g,s in testcases:
-    sol = Solution().findContentChildren(g, s)
+    sol = Solution().findContentChildren2(g, s)
     print(sol)
