33b225aae5ae0db5578726e586409cc5de134944
Kazuki Ueno <ueno@binc.jp>
2025-05-19 10:37:50 +0900

    solve tessoku

---
+++ atcoder/tessoku/a19.py
@@ -0,0 +1,30 @@
+'''
+
+# 問題概要
+
+ナップザック問題
+
+# 考察
+基本っちゃ基本だけどむずいよなぁ。
+a19 と同じように重さを横軸、ものを
+'''
+
+def fprint(dp):
+    for i in dp:
+        print(i)
+
+N, W = map(int,input().split())
+
+DP = [[0 for _ in range(W+1)] for _ in range(N+1)]
+VW = [[0,0]]
+for n in range(N):
+    w,v = map(int,input().split())
+    VW.append([w,v])
+
+for n in range(1,N+1):
+    for w in range(1,W+1):
+        if w < VW[n][0]:
+            DP[n][w] = DP[n-1][w]
+        else:
+            DP[n][w] = max(DP[n-1][w],DP[n-1][w-VW[n][0]] + VW[n][1])
+print(max(DP[N]))
